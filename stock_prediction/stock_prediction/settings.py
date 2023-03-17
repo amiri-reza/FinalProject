9b2c492d2ab280a6e41a87842ff5ff817cdf90f1
amiri-reza <amiri.reza68@yahoo.com>
2023-03-17 16:19:15 +0100

    add allowed hosts

--- stock_prediction/stock_prediction/settings.py
+++ stock_prediction/stock_prediction/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", "NO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["3.76.212.30", "*"]
 
 
 # Application definition
